a1352a62f885773a43f0565bb056619372261711
jorik-1099493 <1099493@hr.nl>
2025-01-31 20:54:27 +0100

    Implemented research model

---
+++ backend/database/models/__init__.py
@@ -0,0 +1,1 @@
+from .research_model import Researches
---
+++ backend/database/models/research_model.py
@@ -0,0 +1,51 @@
+import datetime
+from typing import Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
+
+from backend import db
+
+class Researches(db.Model):
+    """Research Table"""
+
+    research_id: Mapped[int] = mapped_column(primary_key=True)
+    """Unique identifier for the research."""
+
+    title: Mapped[str]
+    """The title of the research study."""
+
+    is_available: Mapped[bool]
+    """Indicates if the research is available."""
+
+    description: Mapped[Optional[str]]
+    """Description of the research."""
+
+    start_date: Mapped[datetime.datetime]
+    """The start date and time of the research."""
+
+    end_date: Mapped[datetime.datetime]
+    """The end date and time of the research."""
+
+    location: Mapped[str]
+    """Location where the research takes place."""
+
+    has_reward: Mapped[bool]
+    """Indicates if there is a reward."""
+
+    reward: Mapped[Optional[str]]
+    """Describes what the reward of this research is."""
+
+    target_min_age: Mapped[Optional[int]]
+    """Minimum age for peer experts."""
+
+    target_max_age: Mapped[Optional[int]]
+    """Maximum age for peer experts."""
+
+    # Define foreign key relationships
+    status_id: Mapped[int] = mapped_column(ForeignKey('research_statuses.research_status_id'))
+    """Foreign key to the research status."""
+
+    research_type_id: Mapped[int] = mapped_column(ForeignKey('research_types.research_type_id'))
+    """Foreign key to the research type."""
