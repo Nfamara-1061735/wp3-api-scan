587950ee6ef6f83c40895f9d954861e783bdbcb7
Hamza <1034556@hr.nl>
2025-02-01 15:02:21 +0100

    added code for debugging researchdata and a function to get clear status

--- backend/database/models/status_model.py
+++ backend/database/models/status_model.py
@@ -6,3 +6,10 @@ class ResearchStatus(db.Model):
 
     research_status_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     status = db.Column(db.String(45), nullable=False)
+
+    # def __repr__(self):
+    #     return f"<ResearchStatus(id={self.research_status_id}, status='{self.status}')>"
+
+    @staticmethod
+    def get_status_by_id(status_id):
+        return db.session.query(ResearchStatus).filter_by(research_status_id=status_id).first()
