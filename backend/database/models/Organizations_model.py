479f73e28554cc2da3e380e4d7a0faecfe9f362f
Tobias-1103787 <1103787@hr.nl>
2025-02-01 18:57:19 +0100

    Made the model which defines the organizations table

---
+++ backend/database/models/Organizations_model.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy import ForeignKey
+
+from backend import db
+
+class Organizations(db.Model):
+    """Organizations Table"""
+    __tablename__ = 'organizations'
+
+    organization_id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True, autoincremental=True)
+    """ID for identifying the organization"""
+
+    name: Mapped[str]
+    """name of organization"""
+
+    website: Mapped[str]
+    """website of organization"""
+
+    description: Mapped[str]
+    """description with """
+
+    contact_person: Mapped[str]
+    """contact person of organization"""
+
+    email: Mapped[str]
+    """email of organization"""
+
+    phone_number: Mapped[str]
+    """phone number of organization"""
+
+    additional_information: Mapped[str]
+    """additional information about organization"""
+
+    organization_types: Mapped[int] = mapped_column(ForeignKey("organisatie_types.id"),nullable=False)
+    """?"""
