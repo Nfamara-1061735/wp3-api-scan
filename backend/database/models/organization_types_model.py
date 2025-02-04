2b609716da26b4e7a9e1487553d97f6950b06d56
Hamza <1034556@hr.nl>
2025-02-04 23:10:06 +0100

    build model for organization_type

---
+++ backend/database/models/organization_types_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend import db
+
+
+class OrganizationType(db.Model):
+    __tablename__ = 'organization_types'
+
+    organization_type_id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True, unsigned=True)
+    """ID from organization type"""
+
+    type: Mapped[str]
+    """defines current type of the organization"""
