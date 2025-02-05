35f9fc6c656ee2523aa9873923e53242b5d0b2ea
jorik-1099493 <1099493@hr.nl>
2025-02-05 12:07:00 +0100

    Added peer expert model to model __init__

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -2,6 +2,7 @@ from .limitations_model import LimitationsModel
 from .organization_types_model import OrganizationType
 from .organizations_model import Organizations
 from .peer_expert_research_type_model import PeerExpertResearchTypeModel
+from .peer_experts_model import PeerExperts
 from .research_limitations_model import ResearchLimitations
 from .research_model import Research
 from .research_types_model import ResearchTypesModel
