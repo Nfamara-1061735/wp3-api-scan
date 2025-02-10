9a3e58466961faa22b3bc418f41dd8fa47d92e90
Nfamara-1061735 <1061735@hr.nl>
2025-02-10 11:53:39 +0100

    init for running the app changed

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 # Create flask instance
-app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
+app = Flask(__name__)
 
 # Config flask application
 app.config['SECRET_KEY'] = "dev"
