fedf2aa301a0e697f16992e232b3b493cecd940c
Nfamara-1061735 <1061735@hr.nl>
2025-03-01 17:36:18 +0100

    bugfix: had click command twice

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, g
 from backend import create_app, db
 from backend.database.models.peer_experts_model import PeerExperts
 
