df453bfe4fdc226e0fa7a52268783608cc3f250a
Hamza <1034556@hr.nl>
2025-03-02 21:10:13 +0100

    added base html code to make a form for registering peer experts

--- frontend/app.py
+++ frontend/app.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from backend import create_app,db
-from backend.database.models.peer_experts_model import PeerExperts
+from flask import render_template, request, redirect, url_for, flash
+from backend import create_app, db
+from backend.database.models.register_expert import ExpertRegistrationModule
 
-# Create flask instance
+
 app = create_app()
 
 @app.route('/')
 def main():
-    return render_template("home.html")
-
+    return render_template('home.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('peer_register.html')
+    elif request.method == 'POST':
+        form_data = request.form  # Haalt alle gegevens op uit het HTML-formulier
+        registration = ExpertRegistrationModule()
+        if registration.register_expert(form_data):
+            flash('Registratie succesvol!', 'success')
+            return redirect(url_for('main'))
+        else:
+            flash('Er is een fout opgetreden tijdens de registratie.', 'danger')
+            return render_template('peer_register.html')
 
 
-# run the app (don't forget to set debug=False when the app is done)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-
     app.run(debug=True)
