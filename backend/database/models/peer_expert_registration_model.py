07cf8fb87cb38c3636786a3cc1f1a816b0d725db
jorik-1099493 <1099493@hr.nl>
2025-03-09 18:36:50 +0100

    Added research requests to peer experts database

---
+++ backend/database/models/peer_expert_registration_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend import db
+
+
+class PeerExpertRegistration(db.Model):
+    __tablename__ = 'peer_expert_registrations'
+
+    peer_expert_registration_id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True)
+    """ID of the registration"""
+
+    registration_status_id: Mapped[int] = mapped_column(ForeignKey('registration_statuses.registration_status_id'))
+    """Foreign key to the status."""
+
+    peer_expert_id: Mapped[int] = mapped_column(ForeignKey('peer_experts.peer_expert_id'))
+    """Foreign key to the target peer expert."""
+
+    research_id: Mapped[int] = mapped_column(ForeignKey('researches.research_id'))
+    """Foreign key to the target research."""
