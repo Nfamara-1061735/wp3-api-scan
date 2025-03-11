7a61d2f5c1cd5c9c0df4c96f66ff6428898b18cb
Tobias-1103787 <1103787@hr.nl>
2025-03-11 22:16:39 +0100

    Editted some JS functions so that it can take multiple data kinds from the DB (W.I.P)

--- frontend/static/js/admin_dashboard.js
+++ frontend/static/js/admin_dashboard.js
@@ -14,38 +14,6 @@ function removeElement(sender) {
         accordionContent.appendChild(p);
     }
 }
-function fetchResearches() {
-    fetch("/researches")
-        .then(response => response.json())
-        .then(data => {
-            console.log("adwd")
-            const researches = document.getElementById('researches');
-            researches.innerHTML = '';
-            data.forEach(research => {
-                const div = document.createElement('div');
-                div.className = 'research';
-                div.setAttribute('data-id', research.research_id);
-
-                if (research.data === '') {div.innerHTML = `
-                            <div class="border rounded-3 p-3">
-                                <p>Geen openstaande onderzoeksvragen</p>
-                            </div>`;} else {div.innerHTML = `
-                            <div class="border rounded-3 p-3">
-                                <p>Titel: ${research.title}</p>
-                                <p>Onderzoeksvraag ID: ${research.research_id}</p>
-                                <button onclick="changeStatus(${research.research_id}, 2)" class="btn btn-success me-2"
-                                        aria-label="Approve expert registration 1">Goedkeuren
-                                </button>
-                                <button onclick="changeStatus(${research.research_id}, 3)" class="btn btn-danger"
-                                        aria-label="Reject expert registration 1">Afkeuren
-                                </button>
-                            </div>`}
-
-                researches.appendChild(div);
-            });
-        })
-        .catch(error => console.error("Error fetching data:", error))
-}
 
 async function changeStatus(item_id, updated_status) {
     try {
@@ -72,3 +40,45 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchResearches();
     setInterval(fetchResearches, 5000);
 })
+
+function fetchResearches() {
+    fetch("/dashboard_data")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                if (item.title === "researches") {
+                    const researches = document.getElementById('researches');
+                    researches.innerHTML = '';
+                    data.forEach(research => {
+                    const div = document.createElement('div');
+                    div.className = 'research';
+                    div.setAttribute('data-id', research.research_id);
+                    if (research.data === '') {div.innerHTML = `
+                                <div class="border rounded-3 p-3">
+                                    <p>Geen openstaande onderzoeksvragen</p>
+                                </div>`;} else {div.innerHTML = `
+                                <div class="border rounded-3 p-3">
+                                    <p>Titel: ${research.title}</p>
+                                    <p>Onderzoeksvraag ID: ${research.research_id}</p>
+                                    <button onclick="changeStatus(${research.research_id}, 2)" class="btn btn-success me-2"
+                                            aria-label="Approve expert registration 1">Goedkeuren
+                                    </button>
+                                    <button onclick="changeStatus(${research.research_id}, 3)" class="btn btn-danger"
+                                            aria-label="Reject expert registration 1">Afkeuren
+                                    </button>
+                                </div>`}
+
+                    researches.appendChild(div);
+                    });
+
+                if (item.title === "peer_expert_registrations") {
+
+                }
+
+                if (item.title === "peer_experts") {
+
+                }
+                }
+            })
+        .catch(error => console.error("Error fetching data:", error))
+})}
