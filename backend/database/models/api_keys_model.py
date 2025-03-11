8ee6d2a3008584feffc86c5c653b60543349092a
Hamza <1034556@hr.nl>
2025-03-11 18:10:59 +0100

    changed api key model to make it more functional

--- backend/database/models/api_keys_model.py
+++ backend/database/models/api_keys_model.py
@@ -1,10 +1,8 @@
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
+from sqlalchemy.orm import Mapped, mapped_column
 from backend import db
 
 class ApiKeys(db.Model):
     __tablename__ = "api_keys"
 
-    api_key: Mapped[int] = mapped_column(primary_key=True)
-    type: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    api_key: Mapped[str] = mapped_column(unique=True, nullable=False)
