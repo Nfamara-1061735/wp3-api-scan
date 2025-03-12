b3b3349cfea4a750d4f857c3a81295d09a4661f7
Nfamara-1061735 <1061735@hr.nl>
2025-03-12 02:51:49 +0100

    changed admin dashboard to make new researches dynamic

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,28 +9,6 @@ class BaseModel(DeclarativeBase):
 
 db:SQLAlchemy = SQLAlchemy(model_class=BaseModel)
 
-def create_app():
-    """Create and configure an instance of the Flask application"""
-    # Create flask instance
-    app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static',static_url_path='/')
-
-
-    # Config flask application
-    app.config['SECRET_KEY'] = "dev"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
-    db.init_app(app)
-
-    app.cli.add_command(init_db_command)
-    app.cli.add_command(init_db_data_command)
-
-    from backend.database import models
-
-    from backend.api import api
-    api.init_app(app)
-
-    return app
-
 @click.command("init-db")
 @with_appcontext
 def init_db_command():
