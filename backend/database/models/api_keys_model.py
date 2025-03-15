71e5ec7892d84c28aead453b7184be9fdb30690a
Hamza <1034556@hr.nl>
2025-03-15 15:00:38 +0100

    added table organisations to api key model

--- backend/database/models/api_keys_model.py
+++ backend/database/models/api_keys_model.py
@@ -6,3 +6,4 @@ class ApiKeys(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     api_key: Mapped[str] = mapped_column(unique=True, nullable=False)
+    organization_name: Mapped[str] = mapped_column(nullable=False)
