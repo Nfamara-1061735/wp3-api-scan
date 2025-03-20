763120d829b1fc8067e7f92e01c55fb40c87444c
Tobias-1103787 <1103787@hr.nl>
2025-03-20 23:10:34 +0100

    Added a function which checks if the peer_expert has an account which is approved (needs further testing)

--- backend/api/login.py
+++ backend/api/login.py
@@ -2,6 +2,7 @@ from flask import request, session
 from flask_restful import Resource, reqparse, abort
 
 from backend.database.models import Users
+from backend.database.models import PeerExperts
 from backend.utils.password import verify_password
 
 
@@ -17,13 +18,33 @@ class Login(Resource):
 
         user: Users = Users.query.filter_by(email=email).first()
 
+        target_status = PeerExperts.query.filter_by(user_id=user.user_id).first().peer_expert_status_id
+
+        if not target_status:
+            abort(422)
+
         # Verify credentials
-        if user and self.verify_role(user, target_role) and verify_password(password, user.password, user.salt):
-            session["user"] = user.user_id  # Storing authentication using a session
-            response = {"success": True}
-            return response
-        else:
-            return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
+        if target_role == 'peer':
+            if (user and self.verify_role(user, target_role)
+                    and verify_password(password, user.password, user.salt)
+                    and self.verify_peer_status(user, target_status)):
+                session["user"] = user.user_id  # Storing authentication using a session
+                response = {"success": True}
+                return response
+            else:
+                if not verify_password(password, user.password, user.salt):
+                    return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
+                if not self.verify_peer_status(user, target_status):
+                    return {"success": False, "message": "Uw account is (nog) niet goedgekeurd."}
+
+        elif target_role == 'admin':
+            if (user and self.verify_role(user, target_role)
+                    and verify_password(password, user.password, user.salt)):
+                session["user"] = user.user_id  # Storing authentication using a session
+                response = {"success": True}
+                return response
+            else:
+                return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
 
     @staticmethod
     def verify_role(user: Users, target_role: str):
@@ -37,3 +58,13 @@ class Login(Resource):
             if user.peer_expert_info:
                 return True
         return False
+
+    @staticmethod
+    def verify_peer_status(user: Users, target_status: int):
+        if not user:
+            return
+
+        if target_status == 2:
+            return True
+        else:
+            return False
