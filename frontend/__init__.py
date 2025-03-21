1bdffc67113f6d2f117817649bb970ca59b7ebce
Nfamara-1061735 <1061735@hr.nl>
2025-03-21 16:36:06 +0100

    changed some small things to fix WCAG

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -134,15 +134,8 @@ def documentation():
     """Toon de API-documentatie, alleen als de API-key geldig is"""
     return render_template("api_documentation.jinja", theme=g.theme)
 
-@frontend_bp.route('/onderzoeken')
-def onderzoeken():
-    alle_onderzoeken = Research.query.all()
-    return render_template("onderzoeken.jinja", theme=g.theme, alle_onderzoeken=alle_onderzoeken)
+# @frontend_bp.route('/onderzoeken')
+# def onderzoeken():
+#     alle_onderzoeken = Research.query.all()
+#     return render_template("onderzoeken.jinja", theme=g.theme, alle_onderzoeken=alle_onderzoeken)
 
-@frontend_bp.route('/beheerders')
-def beheerders():
-    return render_template("beheerders.jinja", theme=g.theme)
-
-@frontend_bp.route('/ervaringsdeskundigen')
-def ervaringsdeskundigen():
-    return render_template("ervaringsdeskundigen.jinja", theme=g.theme)
