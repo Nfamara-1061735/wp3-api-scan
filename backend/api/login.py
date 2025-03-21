38ad57cc054472a81da86875f900e2d9df3b3405
jorik-1099493 <1099493@hr.nl>
2025-03-21 18:23:33 +0100

    Fixed login not working

--- backend/api/login.py
+++ backend/api/login.py
@@ -18,35 +18,35 @@ class Login(Resource):
 
         user: Users = Users.query.filter_by(email=email).first()
 
-        target_status = request.args.get('status', None)
-
         # Verify credentials
         if target_role == 'peer':
-            if (user and self.verify_role(user, target_role)
-                    and verify_password(password, user.password, user.salt)
-                    and self.verify_peer_status(user, target_status)):
-                session["user"] = {
-                'id': user.user_id,
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-            }  # Storing authentication using a session
-                return {"success": True}
-            else:
-                if not verify_password(password, user.password, user.salt):
+            # Check role and password once
+            if user and self.verify_role(user, target_role):
+                if verify_password(password, user.password, user.salt):
+                    print()
+                    if self.verify_peer_status(user):
+                        session["user"] = {
+                            'id': user.user_id,
+                            'email': user.email,
+                            'first_name': user.first_name,
+                            'last_name': user.last_name,
+                        }  # Storing authentication using a session
+                        return {"success": True}
+                    else:
+                        return {"success": False, "message": "Uw account is (nog) niet goedgekeurd."}
+                else:
                     return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
-                if not self.verify_peer_status(user, target_status):
-                    return {"success": False, "message": "Uw account is (nog) niet goedgekeurd."}
+            else:
+                return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
 
         elif target_role == 'admin':
-            if (user and self.verify_role(user, target_role)
-                    and verify_password(password, user.password, user.salt)):
+            if user and self.verify_role(user, target_role) and verify_password(password, user.password, user.salt):
                 session["user"] = {
-                'id': user.user_id,
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-            }  # Storing authentication using a session
+                    'id': user.user_id,
+                    'email': user.email,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                }  # Storing authentication using a session
                 return {"success": True}
             else:
                 return {"success": False, "message": "Verkeerde gebruikersnaam of wachtwoord"}
@@ -65,11 +65,10 @@ class Login(Resource):
         return False
 
     @staticmethod
-    def verify_peer_status(user: Users, target_status: str):
-        if not user:
-            return
-
-        if target_status == '2':
+    def verify_peer_status(user: Users):
+        if not user or not user.peer_expert_info:
+            return False
+        if user.peer_expert_info.peer_expert_status_id == 2:
             return True
         else:
             return False
